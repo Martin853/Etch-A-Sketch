c6866394de62c791c30f6c119ca71f178f84a875
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-01-31 20:10:54 +0100

    Created grid system(failed)

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
 // Firstly make a small div
 // Create a loop that puts the divs into the container
 
+// Main Variables
 const content = document.querySelector(".content");
 const smallDiv = document.createElement("div");
 
+//Small Div Styling
 smallDiv.style.width = "24px";
 smallDiv.style.height = "24px";
 smallDiv.style.border = "red 5px solid";
 
-content.appendChild(smallDiv);
+//Creating the grid
+for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 16; i++) {
+    content.appendChild(smallDiv.cloneNode(true));
+  }
+}
